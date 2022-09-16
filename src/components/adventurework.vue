b9bad854db6c0f795e7108acc0118a628aac6da1
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2022-09-16 22:59:48 +0200

    first commit

--- src/components/adventurework.vue
+++ src/components/adventurework.vue
@@ -3,15 +3,15 @@
         <div class="container text-center">
             <h1>Why book adventure with embark.org ?</h1>
             <div class="row mt-5">
-            <div class="col-4 m-auto">
+            <div class="col-lg-4 col-md-6 col-sm-12 m-auto">
                 <h3 class="w-75 d-inline-block">We do adventure for only adventure</h3>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed sint deserunt odit maxime quo recusandae. Eveniet modi veritatis, molestiae odit nesciunt quae eum neque? Temporibus!</p>
             </div>
-            <div class="col-4 m-auto">
+            <div class="col-lg-4 col-md-6 col-sm-12 m-auto">
                 <h3 class="w-75 d-inline-block">Passionate , responsible , local adventure hosts</h3>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed sint deserunt odit maxime quo recusandae. Eveniet modi veritatis, molestiae odit nesciunt quae eum neque? Temporibus!</p>
             </div>
-            <div class="col-4 m-auto">
+            <div class="col-lg-4 col-md-6 col-sm-12 m-auto">
                 <h3 class="w-50 d-inline-block">Booking a tour is easy</h3>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed sint deserunt odit maxime quo recusandae. Eveniet modi veritatis, molestiae odit nesciunt quae eum neque? Temporibus!</p>
             </div>
